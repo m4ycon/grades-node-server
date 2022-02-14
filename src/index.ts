c661746687e565101f2256ae8c8d77dddfb84ab6
Maycon Fabio <m.vinnicyus@gmail.com>
2022-02-13 23:04:51 -0300

    Setup linter

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express from 'express'
 
 const PORT = 3000
 const HOST = '0.0.0.0'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('Hello World!');
-});
+  return res.send('Hello World!')
+})
 
-app.listen(PORT, HOST, () => console.log(`Running on ${HOST}:${PORT}`));
+app.listen(PORT, HOST, () => console.log(`Running on ${HOST}:${PORT}`))
